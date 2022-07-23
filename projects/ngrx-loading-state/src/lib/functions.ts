c097b8cbc77a6e9376acde3c987e4920a3d6a071
Alan Zhang <alan@lifeready.io>
2022-07-23 14:57:09 +1000

    Added combineLatestLoadingStates

--- projects/ngrx-loading-state/src/lib/functions.ts
+++ projects/ngrx-loading-state/src/lib/functions.ts
@@ -1,3 +1,5 @@
+import { combineLatest, map, Observable } from 'rxjs';
+import { LoadingStateBase } from './loading-state/loading-state-types';
 import { WithLoadingStates } from './types';
 
 export function getInitialState(): WithLoadingStates {
@@ -5,4 +7,20 @@ export function getInitialState(): WithLoadingStates {
     loadingStates: {},
     idLoadingStates: {}
   };
+}
+
+export type CombinedLoadingState = Pick<LoadingStateBase, 'loading' | 'success' | 'error'>;
+
+export function combineLatestLoadingStates(
+  loadingStates$: Observable<LoadingStateBase>[]
+): Observable<CombinedLoadingState> {
+  return combineLatest(loadingStates$).pipe(
+    map((loadingStates) => {
+      return {
+        loading: loadingStates.some((state) => !!state?.loading),
+        success: loadingStates.every((state) => !!state?.success),
+        error: loadingStates.find((state) => !!state?.error)?.error
+      };
+    })
+  );
 }
